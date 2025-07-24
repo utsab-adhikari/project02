0f9a38f556511b628e598cbd076b71a4421f0c20
Utsab Adhikari <utsab1adhikari@gmail.com>
2025-07-24 23:02:12 +0545

    updated

--- src/v2Components/ContextMenu.jsx
+++ src/v2Components/ContextMenu.jsx
@@ -28,9 +28,9 @@ export function ContextMenu({ blogId }) {
       {open && (
         <div className="absolute right-7 top-0 w-36 bg-gray-800 text-white rounded-md shadow-lg z-50">
           <ul className="text-sm">
-            <li className="hover:bg-gray-700 px-4 py-2 cursor-pointer">✏️ Edit</li>
-            <li className="hover:bg-gray-700 px-4 py-2 cursor-pointer">✅ Publish</li>
-            <li className="hover:bg-gray-700 px-4 py-2 cursor-pointer text-red-400">🗑️ Discard</li>
+            <li className="hover:bg-gray-700 border-b border-white px-4 py-2 cursor-pointer">Edit</li>
+            <li className="hover:bg-gray-700 px-4 py-2 border-1/2 border-b border-white cursor-pointer">Publish</li>
+            <li className="hover:bg-gray-700 px-4 py-2 border-b border-white cursor-pointer text-red-400">🗑️ Discard</li>
           </ul>
         </div>
       )}
